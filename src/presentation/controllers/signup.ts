b8fed4be3204311d54cdfb17baefa192242bf649
ryann mesquita <ryann.araujo@gmail.com>
2021-12-19 19:42:35 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,17 +1,18 @@
 import { HttpResponse, HttpRequest } from '../protocols/http';
+import { MissingParamError } from '../errors/missing-param-error';
 
 export class SignUpController {
   handle(_httpRequest: HttpRequest): HttpResponse {
     if (!_httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name'),
+        body: new MissingParamError('name'),
       };
     }
     if (!_httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: email'),
+        body: new MissingParamError('email'),
       };
     }
   }
