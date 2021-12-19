2316a9510ebd1710752cfde2aedcb83ed9a98adf
ryann mesquita <ryann.araujo@gmail.com>
2021-12-19 16:26:13 -0300

    feat: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,16 @@
 export class SignUpController {
   handle(_httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
-    };
+    if (!_httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+    if (!_httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
   }
 }
