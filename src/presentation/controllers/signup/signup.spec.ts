8599bfa1f0db14387319e5d99775480a4f8b7742
Ryann Mesquita <ryann.araujo@gmail.com>
2022-01-27 10:19:59 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -177,9 +177,7 @@ describe('SignUp Controller', () => {
   });
   test('Should return 500 if AddAccount throws', async () => {
     const { sut, addAccountStub } = makeSut();
-    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(async () => {
-      return new Promise((resolve, reject) => { reject(new Error()); });
-    });
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(async () => new Promise((resolve, reject) => { reject(new Error()); }));
     const httpRequest = {
       body: {
         name: 'any_name',
